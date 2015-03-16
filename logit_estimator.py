7a60e31d30bc986d0617d7aa695d9815a7004413
Cooper Oelrichs <c.oelrichs@gmail.com>
2015-03-17 09:22:02 +1000

    added an initial test file for a logit estimator class, with some code to test the python and numpy setup

---
+++ logit_estimator.py
@@ -0,0 +1,9 @@
+# Prototype Logit Estimator
+#
+# This class is a prototype logit estimator, designed for use for discrete choice analysis and modelling
+# Python 3.4 64 bit with SciPy
+
+# testing the creation of an empty matrix in numpy
+import numpy
+zero_mat = numpy.zeros((5,5))
+print(zero_mat)
