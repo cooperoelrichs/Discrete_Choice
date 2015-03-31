965c4c6788014433eb90b204aacbce55acef1e2e
Cooper Oelrichs <c.oelrichs@gmail.com>
2015-03-31 16:59:12 +1000

    Home made logit estimator is now running

--- logit_estimator.py
+++ logit_estimator.py
@@ -12,50 +12,62 @@ class LogitEstimator:
     '''A prototype class for logit estimation'''
     def estimate_scikit_learn_model(data_x, data_y):
         '''Estimate a scikit-learn multinomial logit model'''
+        data_x_mod = numpy.append(numpy.ones((data_x.shape[0], 1)),
+                                  data_x, axis=1)
+
         lr_n = LogisticRegression(dual=False, tol=0.0001,
                                   fit_intercept=True,
                                   class_weight='auto')
-        lr_r = LogisticRegression(penalty='l1', dual=False, tol=0.0001,
-                                  C=20, fit_intercept=True,
-                                  class_weight='auto')
+        # lr_r = LogisticRegression(penalty='l1', dual=False, tol=0.0001,
+        #                           C=20, fit_intercept=True,
+        #                           class_weight='auto')
 
-        lr_n.fit(data_x, data_y)
-        lr_r.fit(data_x, data_y)
+        lr_n.fit(data_x_mod, data_y)
+        # lr_r.fit(data_x, data_y)
         print('Coefficients for MNL')
         print(lr_n.coef_)
-        print('Coefficients for regularised MNL')
-        print(lr_r.coef_)
+        # print('Coefficients for regularised MNL')
+        # print(lr_r.coef_)
 
     def estimate_logit_model(data_x, data_y):
-        LogisticRegressionEstimator.estimate(data_x, data_y)
+        lr = LogisticRegressionEstimator(data_x, data_y)
+        lr.estimate()
 
 
 class LogisticRegressionEstimator:
     '''A home made implimentation of logistic regression'''
-    def estimate(data_x, data_y):
-        theta_initial = 1  # numpy.random.randn()
-        x_mod = numpy.append(numpy.ones((data_x.shape[0], 1)), data_x, axis=1)
-        theta = optimize.fmin_bfgs(cost_function, theta_initial,
-                                   fprime=gradient_function,
-                                   args=(x_mod, data_y))
+    def __init__(self, data_x, data_y):
+        self.data_x = data_x
+        self.data_y = data_y
+        self.theta = numpy.random.randn(self.data_x.shape[1] + 1)
 
-        print('And the paramater is ... ' + theta)
+    def estimate(self):
+        x_mod = numpy.append(numpy.ones((self.data_x.shape[0], 1)),
+                             self.data_x, axis=1)
+        self.theta = optimize.fmin_bfgs(self.cost_function, self.theta,
+                                        fprime=self.gradient_function,
+                                        args=(x_mod, self.data_y))
 
-    def sigmoid(self, data_x):
-        return 1 / (1 + numpy.exp(- data_x))
+        print('And the paramaters are: ')
+        print(self.theta)
 
-    def cost_function(self, theta, data_x, data_y):
-        predicted_probabilities = self.predict_probabilities(data_x, theta)
-        log_likelihood = ((-1 * data_y) * numpy.log(predicted_probabilities) -
-                          (1 - data_y) * numpy.log(1 - predicted_probabilities))
+    def cost_function(self, theta, x, y):
+        # Predict results
+        predictions = numpy.dot(x, theta)
+        # Use the sigmoid function to calculate predicted probabilities
+        predicted_probs = 1 / (1 + numpy.exp(- predictions))
+
+        log_likelihood = ((-1 * y) * numpy.log(predicted_probs) -
+                          (1 - y) * numpy.log(1 - predicted_probs))
         cost = log_likelihood.mean()
         return cost
 
-    def predict_probabilities(self, x, theta):
-        return self.sigmoid(numpy.dot(x, theta))
+    def gradient_function(self, theta, x, y):
+        # Predict results
+        predictions = numpy.dot(x, theta)
+        # Use the sigmoid function to calculate predicted probabilities
+        predicted_probs = 1 / (1 + numpy.exp(- predictions))
 
-    def gradient_function(self, theta, data_x, data_y):
-        predicted_probability = self.sigmoid(numpy.dot(data_x, theta))
-        error = predicted_probability - data_y
-        gradient = numpy.dot(error, data_x) / data_y.size
+        error = predicted_probs - y
+        gradient = numpy.dot(error, x) / y.size
         return gradient
--- logit_runner.py
+++ logit_runner.py
@@ -8,7 +8,7 @@ from logit_estimator import LogitEstimator
 # import pandas
 
 project_location = 'D:\Cooper\Python\Discrete_Choice'
-data_csv_file = project_location + '\\' + 'data_multiclass.csv'
+data_csv_file = project_location + '\\' + 'data.csv'
 
 # Reading the csv using Pandas might be faster
 # data_mat = pandas.io.parsers.read_csv(data_csv_file)
@@ -23,11 +23,11 @@ data_mat = numpy.loadtxt(open(data_csv_file, 'rb'),
 print(headers)
 
 # class_numbers = numpy.transpose(numpy.matrix([1, 2, 3]))
-data_x = data_mat[:, [1, 2, 3, 4, 5, 6, 7, 8]]
-data_y = data_mat[:, 9]
+data_x = data_mat[:, [1, 2, 3, 4, 5, 6, 7]]
+data_y = data_mat[:, 7]
 
-print(data_x)
-print(data_y)
+# print(data_x)
+# print(data_y)
 
 logit_model = LogitEstimator.estimate_scikit_learn_model(data_x, data_y)
 logit_model = LogitEstimator.estimate_logit_model(data_x, data_y)
