128809ad18a6b43f8dc4ddbb69a3193caa1f0488
Cooper Oelrichs <c.oelrichs@gmail.com>
2015-04-22 12:17:46 +1000

    The different estimation methods are now timed

--- logit_runner.py
+++ logit_runner.py
@@ -6,12 +6,13 @@ from sklearn import datasets
 from logit_estimator import LogitEstimator
 from logit_estimator import LogisticRegressionEstimator
 import numpy
+import time
 # import pandas
 
 project_location = 'D:\Cooper\Python\Discrete_Choice'
 data_csv_file = project_location + '\\' + 'data.csv'
 
-X, y = datasets.make_classification(1000, 2, 2, 0, 0, 2)
+X, y = datasets.make_classification(10000, 2, 2, 0, 0, 2)
 C = 0.01
 # print(X)
 # print(y)
@@ -19,21 +20,31 @@ C = 0.01
 scaler = LogitEstimator.scaler(X)
 X_scaled = scaler.transform(X)
 
+start = time.clock()
 lr_scikit = LogitEstimator.estimate_scikit_learn_model(X_scaled, y, C)
+scikit_time = time.clock() - start
+
 X_mod = numpy.append(numpy.ones((X_scaled.shape[0], 1)), X_scaled, axis=1)
 lr_temp = LogisticRegressionEstimator(X_mod, y, C)
 lr_scikit_cost = lr_temp.cost_function(lr_scikit.coef_[0], X_mod, y)
 
+start = time.clock()
 lr_new = LogitEstimator.estimate_home_made_model(X_scaled, y, C)
+new_time = time.clock() - start
 
+start = time.clock()
 lr_new_alt = LogitEstimator.estimate_home_made_model_alt(X_scaled, y, C)
+alt_time = time.clock() - start
 
 print('scikit-learn results')
 print(' - coefficients: ' + str(lr_scikit.coef_[0]))
 print(' - cost: ' + str(lr_scikit_cost))
+print(' - run time: ' + str(scikit_time))
 print('my results')
 print(' - coefficients: ' + str(lr_new.theta))
 print(' - cost: ' + str(lr_new.cost))
+print(' - run time: ' + str(new_time))
 print('my alt results')
 print(' - coefficients: ' + str(lr_new_alt.theta))
 print(' - cost: ' + str(lr_new_alt.cost))
+print(' - run time: ' + str(alt_time))
