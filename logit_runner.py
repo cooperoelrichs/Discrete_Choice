6daf15f52eb39674cc04a6b8e4de8d83d368ea23
Cooper Oelrichs <c.oelrichs@gmail.com>
2015-04-22 10:37:43 +1000

    Moved C to a variable

--- logit_runner.py
+++ logit_runner.py
@@ -12,20 +12,21 @@ project_location = 'D:\Cooper\Python\Discrete_Choice'
 data_csv_file = project_location + '\\' + 'data.csv'
 
 X, y = datasets.make_classification(1000, 2, 2, 0, 0, 2)
+C = 0.01
 # print(X)
 # print(y)
 
 scaler = LogitEstimator.scaler(X)
 X_scaled = scaler.transform(X)
 
-lr_scikit = LogitEstimator.estimate_scikit_learn_model(X_scaled, y, 0.01)
+lr_scikit = LogitEstimator.estimate_scikit_learn_model(X_scaled, y, C)
 X_mod = numpy.append(numpy.ones((X_scaled.shape[0], 1)), X_scaled, axis=1)
-lr_temp = LogisticRegressionEstimator(X_mod, y, 0.01)
+lr_temp = LogisticRegressionEstimator(X_mod, y, C)
 lr_scikit_cost = lr_temp.cost_function(lr_scikit.coef_[0], X_mod, y)
 
-lr_new = LogitEstimator.estimate_home_made_model(X_scaled, y, 0.01)
+lr_new = LogitEstimator.estimate_home_made_model(X_scaled, y, C)
 
-lr_new_alt = LogitEstimator.estimate_home_made_model_alt(X_scaled, y, 0.01)
+lr_new_alt = LogitEstimator.estimate_home_made_model_alt(X_scaled, y, C)
 
 print('scikit-learn results')
 print(' - coefficients: ' + str(lr_scikit.coef_[0]))
