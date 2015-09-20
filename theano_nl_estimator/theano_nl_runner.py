9c2a5bc534a2e1bc65c179781e3b290d6a3002a5
cooperoelrichs <c.oelrichs@gmail.com>
2015-09-21 09:00:15 +1000

    Use the new data loader class

--- theano_nl_estimator/theano_nl_runner.py
+++ theano_nl_estimator/theano_nl_runner.py
@@ -1,8 +1,20 @@
 import numpy as np
-# from sklearn.datasets import load_digits
 from theano_nl_estimator.nested_logit_estimator import NestedLogitEstimator
+from nl_data_loader.nl_data_loader import NLDataLoader
 
-# Setup
+
+dl = NLDataLoader('biogeme_files/swissmetro.dat', '\t',
+                  ['TRAIN_TT', 'TRAIN_CO', 'SM_TT', 'SM_CO', 'CAR_TT', 'CAR_CO'],
+                  'CHOICE')
+dl.data = dl.data[dl.get('CHOICE') != 0]
+dl.data = dl.data[dl.get('SP') != 0]
+dl.data = dl.data[(dl.get('PURPOSE') == 1) | (dl.get('PURPOSE') == 3)]
+dl.print_data_info()
+X, y = dl.get_X_and_y()
+y -= 1
+y = y.astype('int64')
+X /= 100  # scale the costs and travel times
+
 alternatives = 3
 nests = np.array([0, 1], dtype='int32')
 nest_indices = np.array([0, 1, 0])
@@ -11,28 +23,7 @@ lambdas = np.array([2, 1])
 # num_nests = lambdas.shape[0]
 # lambdas_expanded = lambdas[nest_indices]
 
-# digits = load_digits()
-# data_shape = digits.data.shape
-# X = digits.data
-# y = digits.target
-
-file_name = 'biogeme_files/swissmetro.dat'
-data = np.genfromtxt(file_name, delimiter='\t', skip_header=1)
-headers = np.array(open(file_name, 'r').readline().rstrip().split('\t'))
-data = data[data[:, -1] != 0]  # choice != 0
-data = data[data[:, 2] != 0]  # sp
-data = data[(data[:, 4] == 1) | (data[:, 4] == 3)]  # purpose == 1 or purpose == 3
-columns = [18, 19, 21, 22, 25, 26]   # ['TRAIN_TT' 'TRAIN_CO' 'SM_TT' 'SM_CO' 'CAR_TT' 'CAR_CO']
-y = (data[:, -1] - 1).astype('int64')
-X = data[:, columns]
-X /= 100  # scale the costs and travel times
-data_shape = X.shape
-
-print(headers[columns])
-print(np.unique(y))
-print(data_shape)
-
-W_input = np.zeros((data_shape[1], alternatives))  # rand
+W_input = np.zeros((X.shape[1], alternatives))  # rand
 b_input = np.zeros(alternatives)
 # P = np.zeros((data_shape[0], alternatives))
 
