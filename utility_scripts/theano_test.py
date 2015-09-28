91e8e26ffbe55e1559ad464b827f70011cf6f39c
cooperoelrichs <c.oelrichs@gmail.com>
2015-09-28 17:12:01 +1000

    Use tuple, not an array!

--- utility_scripts/theano_test.py
+++ utility_scripts/theano_test.py
@@ -8,5 +8,5 @@ print(X)
 
 
 X = theano.shared(value=np.zeros((2, 2)))
-X = T.set_subtensor(X[[[0, 1], [0, 1]]], np.array([1, 2]))
-X.eval()
+X = T.set_subtensor(X[([0, 1], [0, 1])], np.array([1, 2]))
+print(X.eval())
