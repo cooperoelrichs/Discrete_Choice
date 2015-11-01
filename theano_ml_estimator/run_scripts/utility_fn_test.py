86350566ee92786de2053dadb23d0713f41adf55
cooperoelrichs <c.oelrichs@gmail.com>
2015-11-02 09:00:17 +1000

    Testing actual utility functions

--- theano_ml_estimator/run_scripts/utility_fn_test.py
+++ theano_ml_estimator/run_scripts/utility_fn_test.py
@@ -1,48 +1,100 @@
 import numpy as np
 
 
-X = np.array([[1, 5], [1, 5], [1, 5]])
-draws = np.ones((3, 2, 2)) * 200
-parameters = [2, 0.5, 0.2, 0.001]
-
-print(2 + 1 * 0.5 + 0.2 * 200 + 0.001 * 5 * 200)
-
 parameter_map = {
-    'bias_0': 0,
-    'cost_0': 1,
-    'bias_rp_0': 2,
-    'cost_rp_1': 3,
+    'bias_bicycle': 0,
+    'bias_car': 1,
+    'bias_pt_walk_access': 2,
+    'bias_pt_park_access': 3,
+    'bias_pt_kiss_access': 4,
+    'cost_bicycle': 5,
+    'cost_car': 6,
+    'cost_pt_walk_access': 7,
+    'cost_pt_park_access': 8,
+    'cost_pt_kiss_access': 9,
+    'cost_walk': 10,
+    'random_cost_pt': 11,
+    'random_cost_walk': 12,
+    'random_cost_car': 13,
+    'random_cost_bicycle': 14,
+    'error_pt': 15,
+    'error_car': 16,
+    'error_non_mech': 17,
+    'error_bicycle': 18,
+    # 'error_pt_kiss_access': 19,
 }
 
 draws_map = {
-    'bias_rp_0': 0,
-    'cost_rp_1': 1,
+    'random_cost_pt': 0,
+    'random_cost_walk': 1,
+    'random_cost_car': 2,
+    'random_cost_bicycle': 3,
+    'error_pt': 4,
+    'error_car': 5,
+    'error_non_mech': 6,
+    'error_bicycle': 7,
+    # 'error_pt_kiss_access': 8,
 }
 
 feature_map = {
-    'cost_0': 0,
-    'cost_1': 1,
+    'Bicycle_Cost_Outward': 0,
+    'Bicycle_Cost_Return': 1,
+    'Car_Cost_Outward': 2,
+    'Car_Cost_Return': 3,
+    'WAWE_Cost_Outward': 4,
+    'WAWE_Cost_Return': 5,
+    'PAWE_Cost_Outward': 6,
+    'WAPE_Cost_Return': 7,
+    'KAWE_Cost_Outward': 8,
+    'WAKE_Cost_Return': 9,
+    'Walk_Cost_Outward': 10,
+    'Walk_Cost_Return': 11,
 }
 
+# X[exps, features]
+# draws[exps, biases_rp + parameters_rp, draws]
+# biases[biases]
+# parameters[parameters]
+# biases_rp[biases_rp]
+# parameters_rp[parameters_rp]
+# return[exps, draws]
 
-def v_0(X_, draws_, parameters_):
-    # X[exps, features]
-    # draws[exps, biases_rp + parameters_rp, draws]
-    # biases[biases]
-    # parameters[parameters]
-    # biases_rp[biases_rp]
-    # parameters_rp[parameters_rp]
-    # return[exps, draws]
+def v_bicycle(X_, draws_, parameters_):
+    bias = parameters_[parameter_map['bias_bicycle']]
+    cost = (X_[:, feature_map['Bicycle_Cost_Outward']] * parameters_[parameter_map['cost_bicycle']] +
+            X_[:, feature_map['Bicycle_Cost_Return']] * parameters_[parameter_map['cost_bicycle']])
+    error = (parameters_[parameter_map['error_bicycle']] * draws_[:, draws_map['error_bicycle'], :] +
+             parameters_[parameter_map['error_non_mech']] * draws_[:, draws_map['error_non_mech'], :])
+    random_cost = ((X_[:, feature_map['Bicycle_Cost_Outward']] *
+                    parameters_[parameter_map['random_cost_bicycle']])[:, np.newaxis] *
+                   draws_[:, draws_map['random_cost_bicycle'], :] +
+                   (X_[:, feature_map['Bicycle_Cost_Return']] *
+                    parameters_[parameter_map['random_cost_bicycle']])[:, np.newaxis] *
+                   draws_[:, draws_map['random_cost_bicycle'], :])
+    return bias + cost[:, np.newaxis] + random_cost + error
 
-    bias = parameters_[parameter_map['bias_0']]
-    cost = X_[:, feature_map['cost_0']] * parameters_[parameter_map['cost_0']]
-    bias_rp = parameters_[parameter_map['bias_rp_0']] * draws_[:, draws_map['bias_rp_0'], :]
-    cost_rp = ((X_[:, feature_map['cost_1']] * parameters_[parameter_map['cost_rp_1']])[:, np.newaxis] *
-               draws_[:, draws_map['cost_rp_1'], :])
-    return bias + cost[:, np.newaxis] + bias_rp + cost_rp
+def v_car(X_, draws_, parameters_):
+    bias = parameters_[parameter_map['bias_car']]
+    cost = (X_[:, feature_map['Car_Cost_Outward']] * parameters_[parameter_map['cost_car']] +
+            X_[:, feature_map['Car_Cost_Return']] * parameters_[parameter_map['cost_car']])
+    error = parameters_[parameter_map['error_car']] * draws_[:, draws_map['error_car'], :]
+    random_cost = ((X_[:, feature_map['Car_Cost_Outward']] *
+                    parameters_[parameter_map['random_cost_car']])[:, np.newaxis] *
+                   draws_[:, draws_map['random_cost_car'], :] +
+                   (X_[:, feature_map['Car_Cost_Return']] *
+                    parameters_[parameter_map['random_cost_car']])[:, np.newaxis] *
+                   draws_[:, draws_map['random_cost_car'], :])
+    return bias + cost[:, np.newaxis] + random_cost + error
 
-
-v_output = v_0(X, draws, parameters)
-
-print(v_output)
-print(v_output.shape)
+def v_pt_walk_access(X_, draws_, parameters_):
+    bias = parameters_[parameter_map['bias_pt_walk_access']]
+    cost = (X_[:, feature_map['WAWE_Cost_Outward']] * parameters_[parameter_map['cost_pt_walk_access']] +
+            X_[:, feature_map['WAWE_Cost_Return']] * parameters_[parameter_map['cost_pt_walk_access']])
+    error = parameters_[parameter_map['error_pt']] * draws_[:, draws_map['error_pt'], :]
+    random_cost = ((X_[:, feature_map['WAWE_Cost_Outward']] *
+                    parameters_[parameter_map['random_cost_pt']])[:, np.newaxis] *
+                   draws_[:, draws_map['random_cost_pt'], :] +
+                   (X_[:, feature_map['WAWE_Cost_Return']] *
+                    parameters_[parameter_map['random_cost_pt']])[:, np.newaxis] *
+                   draws_[:, draws_map['random_cost_pt'], :])
+    return bias + cost[:, np.newaxis] + random_cost + error
