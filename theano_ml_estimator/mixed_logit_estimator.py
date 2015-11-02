acd8958e7f5fe875736fe3722ceb6b41c70deefe
cooperoelrichs <c.oelrichs@gmail.com>
2015-11-03 09:08:16 +1000

    Integrating the utility functions

--- theano_ml_estimator/mixed_logit_estimator.py
+++ theano_ml_estimator/mixed_logit_estimator.py
@@ -13,7 +13,7 @@ import time
 
 
 class TheanoMixedLogit(object):
-    def __init__(self):
+    def __init__(self, utility_functions):
         np.seterr(all='raise')
         theano.config.optimizer = 'None'  # 'fast_compile'  # More traceable errors
         theano.config.exception_verbosity = 'high'  # More traceable errors
@@ -21,58 +21,33 @@ class TheanoMixedLogit(object):
         # theano.config.profile = True
         # theano.config.profile_memory = True
 
-        float_type = 'floatX'
+        self.float_type = 'floatX'
         int_type = 'int64'
 
-        self.X = T.matrix('X', dtype=float_type)
-        self.y = T.vector('y', dtype=int_type)
-        self.cost_draws = T.tensor3('cost_draws', dtype=float_type)
-        self.bias_draws = T.tensor3('bias_draws', dtype=float_type)
-        self.W_input = T.matrix('W_input', dtype=float_type)
-        self.b_input = T.vector('b_input', dtype=float_type)
-        self.W_rp_input = T.matrix('W_rp_input', dtype=float_type)
-        self.b_rp_input = T.vector('b_rp_input', dtype=float_type)
-        
-        self.parameters = T.vector('parameters', dtype=float_type)
+        self.utility_functions = utility_functions
 
-        self.costs = T.matrix('costs', dtype=int_type)
-        self.biases = T.matrix('biases', dtype=int_type)
-        self.srng = RandomStreams(seed=2341)
-        self.rp_costs = T.matrix('rp_costs', dtype=int_type)
-        self.rp_biases = T.matrix('rp_biases', dtype=int_type)
-        
-        self.weights = T.vector('weights', dtype=float_type)
+        self.X = T.matrix('X', dtype=self.float_type)
+        self.y = T.vector('y', dtype=int_type)
+        self.draws = T.tensor3('draws', dtype=self.float_type)
+
+        self.parameters = T.vector('parameters', dtype=self.float_type)
+        self.weights = T.vector('weights', dtype=self.float_type)
 
         self.cost, self.error, self.predictions = self.multinomial_logit_cost()
         self.cost_function = self.compile_cost_function()
         self.gradient_function = self.compile_gradient_function()
 
     def multinomial_logit_cost(self):
-        W = T.set_subtensor(self.W_input[(self.costs[:, 0], self.costs[:, 1])],
-                            self.parameters[self.costs[:, 2]])
-        b = T.set_subtensor(self.b_input[self.biases[:, 0]], self.parameters[self.biases[:, 1]])
-
-        W_rp = T.set_subtensor(self.W_rp_input[(self.rp_costs[:, 0], self.rp_costs[:, 1])],
-                               self.parameters[self.rp_costs[:, 2]])
-        b_rp = T.set_subtensor(self.b_rp_input[self.rp_biases[:, 0]], self.parameters[self.rp_biases[:, 1]])
-
         # V = b + w_cost * cost + w_random_cost * cost_draw * cost + w_random_error * error_draw
         # V_rp_cost[14000, 6, 1000] = f(X[14000, costs], W_rp[costs, 6], draws[14000, random_cost_params, 1000])
         # V_rb_bias[14000, 6, 1000] = f(b_rp[6], draws[14000, random_error_params, 1000])
 
+        V, _ = theano.scan(lambda fn, X_, parameters_, draws_: fn(X_, parameters_, draws_),
+                           sequences=self.utility_functions,
+                           outputs_info=T.tensor3('V', dtype=self.float_type),
+                           non_sequences=[self.X, self.parameters, self.draws],
+                           strict=True)
 
-        # Something like this...
-        # V[exps, alt, draws] = f(X[exps, features], W[exps, features, alts, num_draws])
-        # W[exps, features, alts, num_draws] = g(drawn_params[exps, num_params, num_draws], shaper[alts, features])
-        # drawn_params[exps, num_params, num_draws] = h(random_params[num_params], draws[exps, num_params, num_draws])
-
-        V = T.dot(self.X, W) + b
-        W_rp_draws = W_rp[:, :, np.newaxis] * self.cost_draws
-        V_rp_cost = T.dot(self.X, W_rp_draws)
-        V_rp_bias = b_rp[np.newaxis, :, np.newaxis] * self.bias_draws
-        V_rp = (V_rp_cost + V_rp_bias)
-
-        V = V[:, :, np.newaxis] + V_rp
         # P = T.nnet.softmax(V)
         e_V = T.exp(V - V.max(axis=1, keepdims=True))
         P = e_V / e_V.sum(axis=1, keepdims=True)
@@ -85,11 +60,7 @@ class TheanoMixedLogit(object):
 
     def compile_cost_function(self):
         cost_function = theano.function([self.X, self.y,
-                                         self.cost_draws, self.bias_draws,
-                                         self.W_input, self.b_input,
-                                         self.W_rp_input, self.b_rp_input,
-                                         self.costs, self.biases,
-                                         self.rp_costs, self.rp_biases,
+                                         self.draws,
                                          self.parameters,
                                          self.weights],
                                         [self.cost, self.error, self.predictions],
@@ -99,11 +70,7 @@ class TheanoMixedLogit(object):
 
     def compile_gradient_function(self):
         grad_function = theano.function([self.X, self.y,
-                                         self.cost_draws, self.bias_draws,
-                                         self.W_input, self.b_input,
-                                         self.W_rp_input, self.b_rp_input,
-                                         self.costs, self.biases,
-                                         self.rp_costs, self.rp_biases,
+                                         self.draws,
                                          self.parameters,
                                          self.weights],
                                         T.grad(self.cost, wrt=self.parameters),
@@ -113,43 +80,30 @@ class TheanoMixedLogit(object):
 
 
 class MixedLogitEstimator(object):
-    def __init__(self, X, y, W_input, b_input, W_rb_input, b_rp_input,
-                 parameters, costs_params, biases, rp_costs, rp_biases, weights, num_alternatives):
+    def __init__(self, X, y, parameters, utility_functions, weights, num_alternatives):
         self.X = X
         self.y = y
         self.iter = 0
         self.last_time = time.clock()
 
-        # Input params
-        self.W_input = W_input
-        self.b_input = b_input
-        self.W_rp_input = W_rb_input
-        self.b_rp_input = b_rp_input
-
         # Random draws
         self.num_draws = 1000
         self.num_observations = self.X.shape[0]
         self.num_alternatives = num_alternatives
-        self.cost_draws, self.bias_draws = self.generate_random_draws()
 
         self.parameters = parameters
-        self.cost_params = costs_params
-        self.biases = biases
-        self.rp_costs = rp_costs
-        self.rp_biases = rp_biases
+        self.utility_functions = utility_functions
 
+        self.draws = self.generate_random_draws()
         self.weights = weights
 
-        self.W_shape = W_input.shape
-
-        tml = TheanoMixedLogit()
+        tml = TheanoMixedLogit(utility_functions)
         self.cost_function = tml.cost_function
         self.gradient_function = tml.gradient_function
 
     def generate_random_draws(self):
-        cost_draws = -np.log(-np.log(np.random.rand(self.num_observations, self.W_rp_input.shape[0], self.num_draws)))
-        bias_draws = -np.log(-np.log(np.random.rand(self.num_observations, self.b_rp_input.shape[0], self.num_draws)))
-        return cost_draws, bias_draws
+        draws = -np.log(-np.log(np.random.rand(self.num_observations, len(self.parameters), self.num_draws)))
+        return draws
 
     def cost(self, parameters):
         cost, _, _ = self.results(parameters)
@@ -164,11 +118,7 @@ class MixedLogitEstimator(object):
     def results(self, parameters):
         parameters = self.maybe_fix_dtype(parameters)
         cost, error, predictions = self.cost_function(self.X, self.y,
-                                                      self.cost_draws, self.bias_draws,
-                                                      self.W_input, self.b_input,
-                                                      self.W_rp_input, self.b_rp_input,
-                                                      self.cost_params, self.biases,
-                                                      self.rp_costs, self.rp_biases,
+                                                      self.draws,
                                                       parameters,
                                                       self.weights)
         return cost, error, predictions
@@ -176,11 +126,7 @@ class MixedLogitEstimator(object):
     def gradient(self, parameters):
         parameters = self.maybe_fix_dtype(parameters)
         grad = self.gradient_function(self.X, self.y,
-                                      self.cost_draws, self.bias_draws,
-                                      self.W_input, self.b_input,
-                                      self.W_rp_input, self.b_rp_input,
-                                      self.cost_params, self.biases,
-                                      self.rp_costs, self.rp_biases,
+                                      self.draws,
                                       parameters,
                                       self.weights)
         return grad
@@ -202,7 +148,7 @@ class MixedLogitEstimator(object):
         cost, error, predictions = self.results(self.parameters)
         return cost, error, predictions, self.parameters
 
-    def update_random_draws(self, params):
+    def update_random_draws(self):
         elapsed_time = self.get_elapsed_time_and_reset_timer()
         print('Updating draws. Current iter: %i. Iter time: %.0fs' % (self.iter, elapsed_time))
 
@@ -214,14 +160,6 @@ class MixedLogitEstimator(object):
         elapsed_time = current_time - self.last_time
         self.last_time = current_time
         return elapsed_time
-
-    def extract_parameters(self, parameters):
-        W = self.W_input
-        b = self.b_input
-
-        W[(self.cost_params[:, 0], self.cost_params[:, 1])] = self.parameters[self.cost_params[:, 2]]
-        b[self.biases[:, 0]] = parameters[self.biases[:, 1]]
-        return W, b
 
     @staticmethod
     def gradient_check(cost_function, gradient_function, parameters):
---
+++ theano_ml_estimator/run_scripts/utility_functions.py
@@ -0,0 +1,175 @@
+import numpy as np
+import theano
+import theano.tensor as T
+
+
+parameter_map = {
+    'bias_bicycle': 0,
+    'bias_car': 1,
+    'bias_pt_walk_access': 2,
+    'bias_pt_park_access': 3,
+    'bias_pt_kiss_access': 4,
+    'cost_bicycle': 5,
+    'cost_car': 6,
+    'cost_pt_walk_access': 7,
+    'cost_pt_park_access': 8,
+    'cost_pt_kiss_access': 9,
+    'cost_walk': 10,
+    'random_cost_pt': 11,
+    'random_cost_walk': 12,
+    'random_cost_car': 13,
+    'random_cost_bicycle': 14,
+    'error_pt': 15,
+    'error_car': 16,
+    'error_non_mech': 17,
+    'error_bicycle': 18,
+    # 'error_pt_kiss_access': 19,
+}
+
+draw_map = {
+    'random_cost_pt': 0,
+    'random_cost_walk': 1,
+    'random_cost_car': 2,
+    'random_cost_bicycle': 3,
+    'error_pt': 4,
+    'error_car': 5,
+    'error_non_mech': 6,
+    'error_bicycle': 7,
+    # 'error_pt_kiss_access': 8,
+}
+
+feature_map = {
+    'Bicycle_Cost_Outward': 0,
+    'Bicycle_Cost_Return': 1,
+    'Car_Cost_Outward': 2,
+    'Car_Cost_Return': 3,
+    'WAWE_Cost_Outward': 4,
+    'WAWE_Cost_Return': 5,
+    'PAWE_Cost_Outward': 6,
+    'WAPE_Cost_Return': 7,
+    'KAWE_Cost_Outward': 8,
+    'WAKE_Cost_Return': 9,
+    'Walk_Cost_Outward': 10,
+    'Walk_Cost_Return': 11,
+}
+
+# X[exps, features]
+# draws[exps, biases_rp + self.parametersrp, draws]
+# biases[biases]
+# parameters[parameters]
+# biases_rp[biases_rp]
+# self.parametersrp[self.parametersrp]
+# return[exps, draws]
+
+
+class UtilityFunctions(object):
+    def __init__(self, float_dtype):
+        self.X = T.matrix('X', dtype=float_dtype)
+        self.draws = T.tensor3('draws', dtype=float_dtype)
+        self.parameters = T.vector('parameters', dtype=float_dtype)
+
+    def bicycle(self):
+        bias = self.parameters[parameter_map['bias_bicycle']]
+        cost = (self.X[:, feature_map['Bicycle_Cost_Outward']] * self.parameters[parameter_map['cost_bicycle']] +
+                self.X[:, feature_map['Bicycle_Cost_Return']] * self.parameters[parameter_map['cost_bicycle']])
+        error = (self.parameters[parameter_map['error_bicycle']] * self.draws[:, draw_map['error_bicycle'], :] +
+                 self.parameters[parameter_map['error_non_mech']] * self.draws[:, draw_map['error_non_mech'], :])
+        random_cost = ((self.X[:, feature_map['Bicycle_Cost_Outward']] *
+                        self.parameters[parameter_map['random_cost_bicycle']])[:, np.newaxis] *
+                       self.draws[:, draw_map['random_cost_bicycle'], :] +
+                       (self.X[:, feature_map['Bicycle_Cost_Return']] *
+                        self.parameters[parameter_map['random_cost_bicycle']])[:, np.newaxis] *
+                       self.draws[:, draw_map['random_cost_bicycle'], :])
+
+        utility = bias + cost[:, np.newaxis] + random_cost + error
+        fn = theano.function([self.X, self.parameters, self.draws], utility, name='bicycle_utility')
+        return fn
+
+    def car(self):
+        bias = self.parameters[parameter_map['bias_car']]
+        cost = (self.X[:, feature_map['Car_Cost_Outward']] * self.parameters[parameter_map['cost_car']] +
+                self.X[:, feature_map['Car_Cost_Return']] * self.parameters[parameter_map['cost_car']])
+        error = self.parameters[parameter_map['error_car']] * self.draws[:, draw_map['error_car'], :]
+        random_cost = ((self.X[:, feature_map['Car_Cost_Outward']] *
+                        self.parameters[parameter_map['random_cost_car']])[:, np.newaxis] *
+                       self.draws[:, draw_map['random_cost_car'], :] +
+                       (self.X[:, feature_map['Car_Cost_Return']] *
+                        self.parameters[parameter_map['random_cost_car']])[:, np.newaxis] *
+                       self.draws[:, draw_map['random_cost_car'], :])
+
+        utility = bias + cost[:, np.newaxis] + random_cost + error
+        fn = theano.function([self.X, self.parameters, self.draws], utility, name='car_utility')
+        return fn
+
+    def pt_walk_access(self):
+        bias = self.parameters[parameter_map['bias_pt_walk_access']]
+        cost = (self.X[:, feature_map['WAWE_Cost_Outward']] * self.parameters[parameter_map['cost_pt_walk_access']] +
+                self.X[:, feature_map['WAWE_Cost_Return']] * self.parameters[parameter_map['cost_pt_walk_access']])
+        error = self.parameters[parameter_map['error_pt']] * self.draws[:, draw_map['error_pt'], :]
+        random_cost = ((self.X[:, feature_map['WAWE_Cost_Outward']] *
+                        self.parameters[parameter_map['random_cost_pt']])[:, np.newaxis] *
+                       self.draws[:, draw_map['random_cost_pt'], :] +
+                       (self.X[:, feature_map['WAWE_Cost_Return']] *
+                        self.parameters[parameter_map['random_cost_pt']])[:, np.newaxis] *
+                       self.draws[:, draw_map['random_cost_pt'], :])
+
+        utility = bias + cost[:, np.newaxis] + random_cost + error
+        fn = theano.function([self.X, self.parameters, self.draws], utility, name='pt_walk_access_utility')
+        return fn
+
+    def pt_park_access(self):
+        bias = self.parameters[parameter_map['bias_pt_park_access']]
+        cost = (self.X[:, feature_map['PAWE_Cost_Outward']] * self.parameters[parameter_map['cost_pt_park_access']] +
+                self.X[:, feature_map['WAPE_Cost_Return']] * self.parameters[parameter_map['cost_pt_park_access']])
+        error = self.parameters[parameter_map['error_pt']] * self.draws[:, draw_map['error_pt'], :]
+        random_cost = ((self.X[:, feature_map['PAWE_Cost_Outward']] *
+                        self.parameters[parameter_map['random_cost_pt']])[:, np.newaxis] *
+                       self.draws[:, draw_map['random_cost_pt'], :] +
+                       (self.X[:, feature_map['WAPE_Cost_Return']] *
+                        self.parameters[parameter_map['random_cost_pt']])[:, np.newaxis] *
+                       self.draws[:, draw_map['random_cost_pt'], :])
+
+        utility = bias + cost[:, np.newaxis] + random_cost + error
+        fn = theano.function([self.X, self.parameters, self.draws], utility, name='pt_park_access_utility')
+        return fn
+
+    def pt_kiss_access(self):
+        bias = self.parameters[parameter_map['bias_pt_kiss_access']]
+        cost = (self.X[:, feature_map['KAWE_Cost_Outward']] * self.parameters[parameter_map['cost_pt_kiss_access']] +
+                self.X[:, feature_map['WAKE_Cost_Return']] * self.parameters[parameter_map['cost_pt_kiss_access']])
+        error = self.parameters[parameter_map['error_pt']] * self.draws[:, draw_map['error_pt'], :]
+        random_cost = ((self.X[:, feature_map['KAWE_Cost_Outward']] *
+                        self.parameters[parameter_map['random_cost_pt']])[:, np.newaxis] *
+                       self.draws[:, draw_map['random_cost_pt'], :] +
+                       (self.X[:, feature_map['WAKE_Cost_Return']] *
+                        self.parameters[parameter_map['random_cost_pt']])[:, np.newaxis] *
+                       self.draws[:, draw_map['random_cost_pt'], :])
+
+        utility = bias + cost[:, np.newaxis] + random_cost + error
+        fn = theano.function([self.X, self.parameters, self.draws], utility, name='pt_kiss_access_utility')
+        return fn
+
+    def walk(self):
+        cost = (self.X[:, feature_map['Walk_Cost_Outward']] * self.parameters[parameter_map['cost_walk']] +
+                self.X[:, feature_map['Walk_Cost_Return']] * self.parameters[parameter_map['cost_walk']])
+        error = self.parameters[parameter_map['error_non_mech']] * self.draws[:, draw_map['error_non_mech'], :]
+        random_cost = ((self.X[:, feature_map['Walk_Cost_Outward']] *
+                        self.parameters[parameter_map['random_cost_walk']])[:, np.newaxis] *
+                       self.draws[:, draw_map['random_cost_walk'], :] +
+                       (self.X[:, feature_map['Walk_Cost_Return']] *
+                        self.parameters[parameter_map['random_cost_walk']])[:, np.newaxis] *
+                       self.draws[:, draw_map['random_cost_walk'], :])
+
+        utility = cost[:, np.newaxis] + random_cost + error
+        fn = theano.function([self.X, self.parameters, self.draws], utility, name='walk_utility')
+        return fn
+    
+    def fn_list(self):
+        return [
+            self.bicycle(),
+            self.car(),
+            self.pt_walk_access(),
+            self.pt_park_access(),
+            self.pt_kiss_access(),
+            self.walk(),
+        ]
